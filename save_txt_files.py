9478b5e5a283790b364c3ea91edaa1a93e052c31
Zannatun Tania <taniazannatun@gmail.com>
2019-05-10 01:16:43 +0200

    Create save_txt_files.py

---
+++ save_txt_files.py
@@ -0,0 +1,20 @@
+import os
+
+
+def create_txt_file(src):
+    main = os.fsencode(src)
+    f = open('train_names.txt', 'w')
+
+    for folder in os.listdir(main):
+        foldername = src + "/" + os.fsdecode(folder) + "/images/"
+        for file in os.listdir(os.fsencode(foldername)):
+            filename = os.fsdecode(file)
+            if filename.endswith(('.JPEG', '.png', '.jpg')) and not filename.startswith("."):  # image extension we need
+                image = filename
+                f.write(image + '\n')
+    f.close()
+
+
+
+if __name__ == '__main__':
+    create_txt_file('train')
